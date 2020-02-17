6d720eabbd345bc80253bbcbc4c0484c5cd07a7e
acgs771126 <43164653+acgs771126@users.noreply.github.com>
2020-02-17 18:29:51 +0800

    update backup script
    fix s3 path error

--- backup_grafana.sh
+++ backup_grafana.sh
@@ -20,4 +20,4 @@ done
 tar -czvf "${backup_dir}/${timestamp}.tar.gz" ${backup_dir}/{dashboards,datasources,folders,alert_channels}/${timestamp}
 rm -rf ${backup_dir}/{dashboards,datasources,folders,alert_channels}/${timestamp}
 
-aws s3 cp ${backup_dir}/*.tar.gz s3://${S3_BUCKET_NAME}/${S3_BUCKET_KEY}
+aws s3 cp ${backup_dir}/*.tar.gz s3://${S3_BUCKET_NAME}/${S3_BUCKET_KEY}/
